e1cf5dadb189cc49c7b28a49e9c3512a6ada1281
Michelle Giraldo <michellegsld@gmail.com>
2022-10-31 16:19:34 -0400

    Create basic layout with doorway to test user height adjuster, set up canvas and generate button. Time to test if it UI interactions work

---
+++ Assets/Scripts/CanvasSelect.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.Extras;
+
+public class CanvasSelect : MonoBehaviour
+{
+    // Laser pointer I assigned to the right hand
+    public SteamVR_LaserPointer laserPointer;
+
+    // Colors for the button interactions that provides visual cues
+    //public Color defaultColor;
+    //public Color hoverColor;
+    //public Color clickColor;
+
+    private void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e) {
+        //e.target.GetComponent<CanvasButtonT>().buttonColor(hoverColor);
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e) {
+        //e.target.GetComponent<CanvasButtonT>().buttonColor(defaultColor);
+    }
+    
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        //e.target.GetComponent<CanvasButtonT>().buttonColor(clickColor);
+
+    }
+}
